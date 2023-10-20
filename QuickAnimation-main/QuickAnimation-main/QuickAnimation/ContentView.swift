399dc6f7349dcb72cbfa2d7f558f8f0ec4eb52c4
Eli Soares <zandro007@gmail.com>
2023-10-20 13:37:05 -0300

    Add files via upload

---
+++ QuickAnimation-main/QuickAnimation-main/QuickAnimation/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  QuickAnimation
+//
+//  Created by Shreyas Vilaschandra Bhike on 27/05/23.
+//
+
+//  MARK: Instagram
+//  TheAppWizard
+//  MARK: theappwizard2408
+
+import SwiftUI
+
+
+struct ContentView: View {
+   var body: some View {
+       ZStack{
+           TorchView()
+       }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
+
